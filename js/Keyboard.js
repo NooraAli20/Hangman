e3ebf3a851ea71172022f303faea6242a9fcb6f3
NooraAli20 <NooraAli20@gmail.com>
2020-11-03 17:17:07 +0100

    partially implemented the keyboard from which the letters will be matched to the generated words. Also created a randomword.js file that will habor all the potential words

---
+++ js/Keyboard.js
@@ -0,0 +1,94 @@
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
+
+    properties: {
+        value: ""
+    },
+
+    init() {
+        // Create main elements
+        this.elements.main = document.createElement("section");
+        this.elements.keysContainer = document.createElement("section");
+
+        // Setup main elements
+        this.elements.main.classList.add("keyboard", "use-keyboard-input");
+        this.elements.keysContainer.classList.add("keyboard__keys");
+        this.elements.keysContainer.appendChild(this._createKeys());
+
+        this.elements.keys = this.elements.keysContainer.querySelectorAll(".keyboard__key");
+
+        // Add to DOM
+        this.elements.main.appendChild(this.elements.keysContainer);
+        document.body.appendChild(this.elements.main);
+
+        // Automatically use keyboard for elements with .use-keyboard-input
+        document.querySelectorAll(".use-keyboard-input").forEach(element => {
+            element.addEventListener("focus", () => {
+                this.open(element.value, currentValue => {
+                    element.value = currentValue;
+                });
+            });
+        });
+    },
+
+    _createKeys() {
+        const fragment = document.createDocumentFragment();
+        const keyLayout = [
+          "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "å",
+          "a", "s", "d", "f", "g", "h", "j", "k", "l", "ö", "ä",
+          "z", "x", "c", "v", "b", "n", "m"
+      ];
+
+        // Creates HTML for an icon
+        const createIconHTML = (icon_name) => {
+            return `<i class="material-icons">${icon_name}</i>`;
+        };
+
+        keyLayout.forEach(key => {
+            const keyElement = document.createElement("button");
+            const insertLineBreak = ["å", "ä", "m"].indexOf(key) !== -1;
+
+            // Add attributes/classes
+            keyElement.setAttribute("type", "button");
+            keyElement.classList.add("keyboard__key");
+
+
+            keyElement.textContent = key.toUpperCase();
+
+            keyElement.addEventListener("click", () => {
+                console.log(keyElement.innerText);
+            });
+
+            fragment.appendChild(keyElement);
+
+            if (insertLineBreak) {
+                fragment.appendChild(document.createElement("br"));
+            }
+        });
+
+        return fragment;
+    },
+
+    open(initialValue, oninput, onclose) {
+        this.properties.value = initialValue || "";
+        this.eventHandlers.oninput = oninput;
+        this.eventHandlers.onclose = onclose;
+        this.elements.main.classList.remove("keyboard--hidden");
+    },
+
+    close() {
+        this.properties.value = "";
+        this.eventHandlers.oninput = oninput;
+        this.eventHandlers.onclose = onclose;
+        this.elements.main.classList.add("keyboard--hidden");
+    }
+};
