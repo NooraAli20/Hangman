603242b09ab2b0dd93eaa73bfa18b5d2ac708f22
NooraAli20 <NooraAli20@gmail.com>
2020-11-03 12:00:05 +0100

    Implimenteded the keyboard letters

---
+++ js/hangman.js
@@ -0,0 +1,17 @@
+ // Random words of the game
+ var randomWords = {
+   'Sweden',
+   'Uppsala',
+   'Kilimanjaro' 
+ }
+ document.querySelector('#btn').addEventListener('click', 
+ function() {
+         let qwertyKeyboard =
+                    QWERTYUIOPÅ
+                    ASDFGHJKLÖÄ
+                    ZXCVBNM
+
+})
+
+                
+       
