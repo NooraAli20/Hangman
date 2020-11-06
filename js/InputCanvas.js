48bcbe95c93876e0175039ae27043996277cbd1f
NooraAli20 <malehakim@gmail.com>
2020-11-06 22:39:15 +0100

    Edited keyboard to respond to different events

--- js/InputCanvas.js
+++ js/InputCanvas.js
@@ -46,6 +46,7 @@ const ButtonsCanvas = {
 
             // Add attributes/classes
             buttonElement.setAttribute("type", "button");
+            buttonElement.style.cursor = "pointer";
             buttonElement.classList.add("keyboard__key");
 
             // Make the text of the button to be uppercase
@@ -68,22 +69,32 @@ const ButtonsCanvas = {
                         element.classList.remove('lousy')
                         element.classList.add('visualizeKey');
                         element.textContent = buttonElement.innerText;
-
-                        // update the score for each right letter chosen
-                        RandomWords.locals.score++;
+                        buttonElement.style.color = 'red';
+                        buttonElement.disabled = true;
                     });
                 }
                 else
                 {
-                    RandomWords.locals.currentWeightAggregate += RandomWords.locals.weightOfEachCharacterPerWord;
-                    RandomWords.calculateImageToUpdate(RandomWords.locals.currentWeightAggregate);
+                    // If we are on the finaly image, 5th image, 
+                    if(RandomWords.locals.imageOnDisplay == 5)
+                    {
+                        let wordHTML = document.querySelectorAll('.square.lousy');
+                        wordHTML.forEach(article => {
+                           article.textContent = article.classList[2];
+                           article.style.color = 'red';
+                           article.classList.add('squareReveal');
+                        });
+                    }
+                    else if(RandomWords.locals.imageOnDisplay < 5)
+                    {
+                        buttonElement.style.color = 'rgba(210, 136, 136, 0.5)';
+                        buttonElement.style.background = 'rgba(235, 234, 233, 0.9)';
+                        buttonElement.title = 'Letter disabled. Already used';
+                        buttonElement.disabled = true;
+                        RandomWords.updateImageOnFailedAttempt(RandomWords.locals.imageOnDisplay);
+                        RandomWords.locals.imageOnDisplay++;
+                    }
                 }
-               
-                // Update the attempts made so far
-                RandomWords.locals.numberOfAttemptsDone++;
-
-                // Render the <p></p> to re-update the attemps and scores sheet
-                document.querySelector('.scores').textContent = RandomWords.attempsAndScoresMessage();
 
                 // Now check if there is any .square element with the lousy class attached to it. If not any, then the 
                 // user has finished the game. Ask if he wants to play again and generate the markup again
@@ -118,13 +129,10 @@ const RandomWords = {
         word: [],
         charsInWord: 0,
         levelOfDifficulty: 2,
-        weightOfEachCharacterPerWord: 0.0,
         imageBaseUrl: '../images/',
         currentWeightAggregate: 0.0,
         imagesExt: '.png',
-        currentImageOnDisplay: 0,
-        previousImageOnDisplay: 0,
-        numberOfAttemptsDone: 0,
+        imageOnDisplay: 1,
         totalNumberOfAttemps: 0,
         score: 0
     },
@@ -145,81 +153,20 @@ const RandomWords = {
         this.locals.word = splitRandomChosenWord;
         this.locals.charsInWord = splitRandomChosenWord.length;
 
-        // Get the weight as a percentage
-        this.locals.weightOfEachCharacterPerWord = (1 / this.locals.charsInWord) * 100;
-
-        // Give the number of available attempts. 
-        this.locals.totalNumberOfAttemps = this.locals.charsInWord + 2;
-
-        // Since we have 5 pictures to display from the /images folder, that means each character will minimamly have 20% weight
-        // But we have to cater for words whose character weighs more than 20%, i.e words with atmost 5 characters
-        if(this.locals.charsInWord <= 5)
-        {
-            this.locals.weightOfEachCharacterPerWord = (1 / (this.locals.charsInWord + 6) ) * 100;
-        }
-
-        console.log(this.locals.weightOfEachCharacterPerWord);
-
         splitRandomChosenWord.forEach(character => {
             var letter = document.createElement("article");
             letter.classList.add("square");
             letter.classList.add("lousy");
+            letter.style.color = 'black';
             letter.classList.add(character.toUpperCase());
             letter.textContent = " ";
             choosenWordPlaceHolder.appendChild(letter);
         });
         
         document.querySelector('.container').appendChild(choosenWordPlaceHolder);
-        this.generateAttemptsAndScoreSection();
-    },
-    generateAttemptsAndScoreSection()
-    {
-        let secktion = document.createElement('section');
-        secktion.classList.add('centralizeLabelAndScore');
-
-        let label = document.createElement('p');
-        label.classList.add('scores');
-        
-        label.textContent = this.attempsAndScoresMessage();;
-
-        secktion.appendChild(label);
-
-        document.querySelector('.container').appendChild(secktion);
-    },
-    calculateImageToUpdate(y)
-    {
-        if(y > 0 && y <= 20)
-        {
-            this.currentImageOnDisplay = 1;
-        }else if(y > 20 && y <= 40)
-        {
-            this.currentImageOnDisplay = 2;
-        }
-        else if(y > 40 && y <= 60)
-        {
-            this.currentImageOnDisplay = 3;
-        }
-        else if(y > 60 && y <= 80)
-        {
-            this.currentImageOnDisplay = 4;
-        }
-        else if(y > 80)
-        {
-            this.currentImageOnDisplay = 5;
-        }
-
-        this.updateImageOnFailedAttempt(this.currentImageOnDisplay);
-        this.previousImageOnDisplay = this.currentImageOnDisplay;
     },
     updateImageOnFailedAttempt(imageNumber)
     {
-        if(this.currentImageOnDisplay !== this.previousImageOnDisplay)
-            document.querySelector('#suicideImage').setAttribute('src', this.locals.imageBaseUrl + imageNumber + this.locals.imagesExt);
-    },
-    attempsAndScoresMessage()
-    {
-        return 'Attempts ' + 
-                this.locals.numberOfAttemptsDone + '/' + 
-                this.locals.totalNumberOfAttemps +  ' - Score : ' + this.locals.score +  ' pts';
+        document.querySelector('#suicideImage').setAttribute('src', this.locals.imageBaseUrl + imageNumber + this.locals.imagesExt);
     }
 }
