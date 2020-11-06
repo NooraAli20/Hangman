ad4e4f9be2f7c1e30e6f208c9a83d9107a93a5ed
NooraAli20 <malehakim@gmail.com>
2020-11-06 10:57:13 +0100

    Latest updates

--- js/hangman.js
+++ js/hangman.js
@@ -1,31 +1,8 @@
 window.addEventListener("DOMContentLoaded", function () {
    // Initialize the keyboard
-    Keyboard.init();
+  ButtonsCanvas.init();
 
-    generateAndRenderSampleWord();
+  RandomWords.generateAndRenderSampleWord();
     
 });
 
-function generateAndRenderSampleWord()
-{
-  // Randomly choose a word
-  var arrayOfWords = randWords.words;
-  var randomWordChoosen = arrayOfWords[Math.floor(Math.random() * arrayOfWords.length)];
-
-  let choosenWordPlaceHolder = document.createElement("section");
-  choosenWordPlaceHolder.classList.add("placeHolderChoosen");
-
-  let splitRandomChosenWord = randomWordChoosen.trim().split('');
-
-  splitRandomChosenWord.forEach(character => {
-    let letter = document.createElement("article");
-    letter.classList.add("square");
-    letter.classList.add("lousy");
-    letter.classList.add(character.toUpperCase());
-    letter.textContent = " ";
-    choosenWordPlaceHolder.appendChild(letter);
- });
-
-  //document.body.appendChild(choosenWordPlaceHolder);
-  document.querySelector('.container').appendChild(choosenWordPlaceHolder);
-}
